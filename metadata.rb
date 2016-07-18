6f1f5d9e37066c20af7ca6a69d29d0abca06ff18
Ramez Hanna <r.hanna@criteo.com>
2016-07-18 10:21:19 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jarosser06@gmail.com'
 license          'MIT'
 description      'Installs/Configures sysdig'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.3.2'
 depends          'apt'
 depends          'yum'
 depends          'yum-epel'
